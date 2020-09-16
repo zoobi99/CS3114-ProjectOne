90bc2645fbefa2be7b32e3b4185e07a6af86e4c8
Zoobi <Zoobi@vt.edu>
2020-09-16 14:34:43 -0400

    fixed case involving new update having a higher quality grade

--- src/CovidParser.java
+++ src/CovidParser.java
@@ -5,101 +5,120 @@ import java.io.IOException;
 
 public class CovidParser {
 
-	private CovidUpdate[] masterList;
-	private int localIndex;
-	
-	public CovidParser(String commandFileName) throws IOException {
-	
-		masterList = new CovidUpdate[100];
-		File file = new File(commandFileName);
-		String row = "";
-		BufferedReader csvReader = new BufferedReader(new FileReader(file));
+    private CovidUpdate[] masterList;
+    private int localIndex;
+
+
+    public CovidParser(String commandFileName) throws IOException {
+
+        masterList = new CovidUpdate[100];
+        File file = new File(commandFileName);
+        String row = "";
+        BufferedReader csvReader = new BufferedReader(new FileReader(file));
         csvReader.readLine();
         localIndex = 0;
         // Pulling off each row of data
         while ((row = csvReader.readLine()) != null) {
-        	// Handle case of missing line
-        	if (row.equals(",,,,,,,,,")) {
-        		continue;
-        	}
+            // Handle case of missing line
+            if (row.equals(",,,,,,,,,")) {
+                continue;
+            }
             String[] data = row.split(",");
             for (int i = 0; i < data.length; i++) {
-            	if (data[i].length() == 0) {
-            		data[i] = "-1";
-            	}
+                if (data[i].length() == 0) {
+                    data[i] = "-1";
+                }
             }
-            CovidUpdate update = new CovidUpdate(Integer.parseInt(data[0]), data[1],
-            		(int)Double.parseDouble(data[2]), (int)Double.parseDouble(data[3]), 
-            		(int)Double.parseDouble(data[4]), (int)Double.parseDouble(data[5]), 
-            		(int)Double.parseDouble(data[6]), (int)Double.parseDouble(data[7]),
-            		data[8], (int)Double.parseDouble(data[9]), localIndex);
-            // Checking if state is valid, will continue to next iteration if not
+            CovidUpdate update = new CovidUpdate(Integer.parseInt(data[0]),
+                data[1], (int)Double.parseDouble(data[2]), (int)Double
+                    .parseDouble(data[3]), (int)Double.parseDouble(data[4]),
+                (int)Double.parseDouble(data[5]), (int)Double.parseDouble(
+                    data[6]), (int)Double.parseDouble(data[7]), data[8],
+                (int)Double.parseDouble(data[9]), localIndex);
+            // Checking if state is valid, will continue to next iteration if
+            // not
             if (!update.checkState(data[1], false)) {
-            	continue;
+                continue;
             }
-            // Checking if there is currently an update with the same name and date
+            // Checking if there is currently an update with the same name and
+            // date
             for (int i = 0; i < localIndex; i++) {
-            	if (masterList[i].getDate() == update.getDate() && 
-            			masterList[i].getState().name().equals(update.getState().name())) {
-            		masterList[i] = this.compareEntries(masterList[i], update);
-            		continue;
-            	}
+                if (masterList[i].getDate() == update.getDate() && masterList[i]
+                    .getState().name().equals(update.getState().name())) {
+                    masterList[i] = this.compareEntries(masterList[i], update);
+                    continue;
+                }
             }
             masterList[localIndex] = update;
-            localIndex++;		
+            localIndex++;
         }
         csvReader.close();
-	}
-	
-	private CovidUpdate compareEntries(CovidUpdate current, CovidUpdate newUpdate) {
-		boolean updateFlag = false;
-		if (newUpdate.getDataQualityScore() > current.getDataQualityScore()) {
-			updateFlag = true;
-		}
-		int date = current.getDate();
-		String state = current.getState().name();
-		// Checking for missing values in current
-		int positiveCases = current.getPositives();
-		if (positiveCases == -1 || updateFlag) {
-			positiveCases = newUpdate.getPositives();
-		}
-		int negativeCases = current.getNegatives();
-		if (negativeCases == -1 || updateFlag) {
-			negativeCases = newUpdate.getNegatives();
-		}
-		int hospitalized = current.getHospitalized();
-		if (hospitalized == -1 || updateFlag) {
-			hospitalized = newUpdate.getHospitalized();
-		}
-		int currOnVent = current.getCurrentOnVent();
-		if (currOnVent == -1 || updateFlag) {
-			currOnVent = newUpdate.getCurrentOnVent();
-		}
-		int cumOnVent = current.getCumulativeOnVent();
-		if (cumOnVent == -1 || updateFlag) {
-			cumOnVent = newUpdate.getCumulativeOnVent();
-		}
-		int recovered = current.getRecovered();
-		if (recovered == -1 || updateFlag) {
-			recovered = newUpdate.getRecovered();
-		}
-		String grade = current.getDataQualityGrade();
-		if (updateFlag) {
-			grade = newUpdate.getDataQualityGrade();
-		}
-		int deaths = current.getDeaths();
-		if (deaths == -1 || updateFlag) {
-			deaths = newUpdate.getDeaths();
-		}
-		// we will keep the old index
-		int updateIndex = current.getIndex();
-		
-		CovidUpdate output = new CovidUpdate(date, state, positiveCases, negativeCases,
-				hospitalized, currOnVent, cumOnVent, recovered, grade, deaths, updateIndex);
-		return output;
-	}
+    }
 
-	public CovidUpdate[] getList() {
-		return this.masterList;
-	}
+
+    private CovidUpdate compareEntries(
+        CovidUpdate current,
+        CovidUpdate newUpdate) {
+        boolean updateFlag = false;
+
+        if (newUpdate.getDataQualityScore() > current.getDataQualityScore()) {
+            updateFlag = true;
+        }
+        int date = current.getDate();
+        String state = current.getState().name();
+        // Checking for missing values in current and ensures 
+        // newUpdate doesn't have missing data
+        int positiveCases = current.getPositives();
+        int newPositiveCases = newUpdate.getPositives();
+        if ((positiveCases == -1 || updateFlag) && newPositiveCases != -1) {
+            positiveCases = newUpdate.getPositives();
+        }
+        int negativeCases = current.getNegatives();
+        int newNegativeCases = newUpdate.getNegatives();
+        if ((negativeCases == -1 || updateFlag) && newNegativeCases != -1) {
+            negativeCases = newUpdate.getNegatives();
+        }
+        int hospitalized = current.getHospitalized();
+        int newHospitalized = newUpdate.getHospitalized();
+        if ((hospitalized == -1 || updateFlag) && newHospitalized != -1) {
+            hospitalized = newUpdate.getHospitalized();
+        }
+        int currOnVent = current.getCurrentOnVent();
+        int newOnVentilator = newUpdate.getCurrentOnVent();
+        if ((currOnVent == -1 || updateFlag) && newOnVentilator != -1) {
+            currOnVent = newUpdate.getCurrentOnVent();
+        }
+        int cumOnVent = current.getCumulativeOnVent();
+        int newCumOnVent = newUpdate.getCumulativeOnVent();
+        if ((cumOnVent == -1 || updateFlag) && newCumOnVent != -1) {
+            cumOnVent = newUpdate.getCumulativeOnVent();
+        }
+        int currRecovered = current.getRecovered();
+        int newRecovered = newUpdate.getRecovered();
+        if ((currRecovered == -1 || updateFlag) && newRecovered != -1) {
+            currRecovered = newUpdate.getRecovered();
+        }
+        String grade = current.getDataQualityGrade();
+        if (updateFlag) {
+            grade = newUpdate.getDataQualityGrade();
+        }
+        int deaths = current.getDeaths();
+        int newDeaths = newUpdate.getDeaths();
+        if ((deaths == -1 || updateFlag) && newDeaths != -1) {
+            deaths = newUpdate.getDeaths();
+        }
+        // we will keep the old index
+        int updateIndex = current.getIndex();
+
+        CovidUpdate output = new CovidUpdate(date, state, positiveCases,
+            negativeCases, hospitalized, currOnVent, cumOnVent, currRecovered,
+            grade, deaths, updateIndex);
+        return output;
+    }
+
+
+    public CovidUpdate[] getList() {
+        return this.masterList;
+    }
+
 }
